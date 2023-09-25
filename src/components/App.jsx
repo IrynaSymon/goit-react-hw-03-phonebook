eda793d99cfddfc45990e99a93d82d10527e7d58
IrynaSymon <126278348+IrynaSymon@users.noreply.github.com>
2023-09-25 21:02:17 +0200

    pravka

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,7 @@ import { Section } from './Section/Section';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -83,13 +78,15 @@ export class App extends Component {
         <Section title="Phonebook">
           <ContactForm onSubmit={this.addContact} />
         </Section>
-        <Section title="Contacts">
-          <Filter value={filter} onChange={this.handleFilter} />
-          <ContactList
-            contacts={visibleContacts}
-            onDeleteContact={this.deleteContact}
-          />
-        </Section>
+        {this.state.contacts.length >= 1 && (
+          <Section title="Contacts">
+            <Filter value={filter} onChange={this.handleFilter} />
+            <ContactList
+              contacts={visibleContacts}
+              onDeleteContact={this.deleteContact}
+            />
+          </Section>
+        )}
       </div>
     );
   }
